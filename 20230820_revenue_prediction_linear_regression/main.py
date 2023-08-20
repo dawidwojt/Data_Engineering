5921bf074abb0c3f4a37fdb7732d5f6f5505c838
Dawid Wojtasiński <wojtawid@gmail.com>
2023-08-20 21:59:35 +0200

    lr

--- 20230820_revenue_prediction_linear_regression/main.py
+++ 20230820_revenue_prediction_linear_regression/main.py
@@ -1,26 +1,28 @@
-# % pip install scikit-learn
-# % pip install pandas
-# % pip install matplotlib
-# or use:
 # % pip install -r requirements.txt
+# or install one by one:
+    # % pip install scikit-learn
+    # % pip install pandas
+    # % pip install matplotlib
+
 
 from model import Prediction
 import numpy as np
 import pandas as pd
+import csv
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
+from time import sleep
 
-
-def make_prediction(inputs: list[float], outputs: list[float], input_value: float, plot: bool = False) -> Prediction:
+def make_prediction(inputs: list[float], outputs: list[float], input_value: float, c_name: str,  plot: bool = False ) -> Prediction:
     if len(inputs) != len(outputs):
         raise Exception('Length of inputs and outputs need to match')
 
     """Creating a dataframe for the data"""
     df = pd.DataFrame({'inputs': inputs, 'outputs': outputs})
 
-    """Reshaping it using Numpy"""
+    """Reshaping it using Numpy to have it in a vertical position"""
     X = np.array(df['inputs']).reshape(-1,1)
     y = np.array(df['outputs']).reshape(-1,1)
 
@@ -40,7 +42,7 @@ def make_prediction(inputs: list[float], outputs: list[float], input_value: floa
 
     """PLot"""
     if plot:
-        raise display_plot(inputs=X, outputs=y, y_line=y_line)
+        display_plot(inputs=X, outputs=y, c_name=c_name, y_line=y_line)
     return Prediction(value=y_prediction[0][0],
                       r2_score=r2_score(test_y, y_test_prediction),
                       slope=model.coef_[0][0],
@@ -48,18 +50,38 @@ def make_prediction(inputs: list[float], outputs: list[float], input_value: floa
                       mean_absolute_error=mean_absolute_error(test_y, y_test_prediction))
 
 
-def display_plot(inputs: list[float], outputs: list[float], y_line):
-    plt.scatter(inputs, outputs, s=12)
+def display_plot(inputs: list[float], outputs: list[float], c_name: str, y_line):
+    plt.scatter(inputs, outputs, s=20)
     plt.xlabel('Inputs')
     plt.ylabel('Outputs')
     plt.plot(inputs, y_line, color='r')
-    plt.show()
+    plt.title(c_name)
+    plt.savefig(f'{c_name}_plot.png')
+    plt.clf()
 
 
-if __name__ == '__main__':
-    years: list[int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    revenue: list[int] = [100, 121, 91, 140, 202, 190, 191, 251, 299, 389, 380, 450, 478, 601, 577]
-    my_input: int = 20
-    prediction: Prediction = make_prediction(inputs=years, outputs=revenue, input_value=my_input, plot=True)
-    print('Input: ', my_input)
-    print(prediction)
+
+def main():
+    my_input: int = int(input('PLease provide a year to predict revenue: '))
+
+
+    with open("revenue_data.csv", newline='') as revenue_data:
+        reader = csv.reader(revenue_data)
+
+        years: list[int] = next(reader)[1:]
+        years = [int(s) for s in years] # gets first row
+        data_dict: dict = {}
+        for row in reader:
+            name: str = row[0]
+            values: list[int] = row[1:]
+            values = [int(s) for s in values]
+            data_dict[name] = values
+    for company in data_dict:
+        name: str = company
+        revenue: list[int] = (data_dict[name])
+        print(f"Company: {name.upper()} ")
+        prediction: Prediction = make_prediction(inputs=years, outputs=revenue, c_name=company, input_value=my_input, plot=True)
+        print('Input: ', my_input)
+        print(prediction)
+
+main()
